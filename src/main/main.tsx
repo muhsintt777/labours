318c7adc0385ad677134a8675cd129a81f14f8bc
Muhsin <muhsintt777@gmail.com>
2024-06-12 21:03:29 +0530

    fix : import store

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,13 +1,10 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { App } from "./App.tsx";
-// import { Provider } from "react-redux";
-// import { store } from "configs/store.ts";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store } from "store/store";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -18,5 +15,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </Routes>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
